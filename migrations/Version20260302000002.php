3b6b6586036422b4012daa7752b5a5abf779708a
dlange97 <fejkowe21@gmail.com>
2026-03-16 21:31:15 +0100

    fix migration

--- migrations/Version20260302000002.php
+++ migrations/Version20260302000002.php
@@ -8,7 +8,6 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
  */
 final class Version20260302000002 extends AbstractMigration
 {
